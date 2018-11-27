7f927d570ad61278e7a27d8d3a51ffc3f661e2cf
Allen <39018805+wangcxallen@users.noreply.github.com>
2018-11-26 20:01:00 -0500

    Add files via upload

--- src/dw1000_tx.c
+++ src/dw1000_tx.c
@@ -14,6 +14,8 @@
  /*
  * Modified by:
  * Anh Luong <luong@eng.utah.edu>
+ * Further Modified by:
+ * Chenxi Wang <chenxiwa@andrew.cmu.edu>
  */
 
 #include <stdio.h>
@@ -25,7 +27,7 @@
 #include "platform.h"
 
 /* Example application name and version to display on LCD screen. */
-#define APP_NAME "SIMPLE TX v1.2"
+#define APP_NAME "HEADCOUNT TX v1.0"
 
 /* Default communication configuration. We use here EVK1000's default mode (mode 3). */
 static dwt_config_t config = {
@@ -48,16 +50,36 @@ static dwt_config_t config = {
  *     - byte 10/11: frame check-sum, automatically set by DW1000.  */
 static uint8 tx_msg[] = {0xC5, 0, 'D', 'E', 'C', 'A', 'W', 'A', 'V', 'E', 0, 0};
 /* Index to access to sequence number of the blink frame in the tx_msg array. */
-#define BLINK_FRAME_SN_IDX 1
+#define BLINK_FRAME_SN_IDX 1   //not used
+#define TS_IDX   2   // time_stamp index
 
 /* Inter-frame delay period, in milliseconds. */
 #define TX_DELAY_MS 2000
+
+/***** Function declarations *****/
+
+static uint64 get_tx_timestamp_u64(void);
+static uint64 get_system_timestamp_u64(void);
+
 
 /**
  * Application entry point.
  */
 int main(void)
 {
+    uint32 exchangeNo = 0;
+    uint32 frame_len = 0;
+    uint32 status_reg = 0;
+    int ret = 0;
+    uint64 time = 0;
+    
+    /* The frame sent in this example is adjusted from an 802.15.4e standard blink. It is a 12-byte frame composed of the following fields:
+     *     - byte 0: frame type (0xC5 for a blink).
+     *     - byte 1: sequence number, incremented for each new frame.
+     *     - byte 2 -> 9: tx_timestamp
+     *     - byte 10/11: frame check-sum, automatically set by DW1000.  */
+    uint8 tx_msg[] = {0xab, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // size = 1+1+8+2 = 12
+    
     /* Start with board specific hardware init. */
 	hardware_init();
 
@@ -82,6 +104,10 @@ int main(void)
     /* Loop forever sending frames periodically. */
     while(1)
     {
+        /* Get tx_timestamp */
+        time = get_system_timestamp_u64();
+        
+        memcpy((void *) &tx_msg[TS_IDX], (void *) &time, sizeof(uint64)); // copy tx timestamp
         /* Write frame data to DW1000 and prepare transmission. See NOTE 4 below.*/
         dwt_writetxdata(sizeof(tx_msg), tx_msg, 0); /* Zero offset in TX buffer. */
         dwt_writetxfctrl(sizeof(tx_msg), 0, 0); /* Zero offset in TX buffer, no ranging. */
@@ -104,9 +130,62 @@ int main(void)
         /* Increment the blink frame sequence number (modulo 256). */
         tx_msg[BLINK_FRAME_SN_IDX]++;
 
-        printf("MSG SENT!\n");
+        /* How to print uint8??????? */
+        printf("%u MSG SENT!\n", tx_msg[BLINK_FRAME_SN_IDX]-1);
     }
 }
+
+/*! ------------------------------------------------------------------------------------------------------------------
+ * @fn get_tx_timestamp_u64()
+ *
+ * @brief Get the TX time-stamp in a 64-bit variable.
+ *        /!\ This function assumes that length of time-stamps is 40 bits, for both TX and RX!
+ *
+ * @param  none
+ *
+ * @return  64-bit value of the read time-stamp.
+ */
+static uint64 get_tx_timestamp_u64(void)
+{
+    uint8 ts_tab[5];
+    uint64 ts = 0;
+    int i;
+    dwt_readtxtimestamp(ts_tab);
+    for (i = 4; i >= 0; i--)
+    {
+        ts <<= 8;
+        ts |= ts_tab[i];
+    }
+    return ts;
+}
+
+/*! ------------------------------------------------------------------------------------------------------------------
+ * @fn get_system_timestamp_u64()
+ *
+ * @brief Get the system in a 64-bit variable.
+ *
+ * @param  none
+ *
+ * @return  64-bit value of the system time.
+ */
+static uint64 get_system_timestamp_u64(void)
+{
+    uint8 ts_tab[5];
+    uint64 ts = 0;
+    int i;
+    dwt_readsystime(ts_tab);
+    for (i = 4; i >= 0; i--)
+    {
+        ts <<= 8;
+        ts |= ts_tab[i];
+    }
+    return ts;
+}
+
+
+
+
+
 
 /*****************************************************************************************************************************************************
  * NOTES:
